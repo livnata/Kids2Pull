7aac6f8dc5ff49c11f9cca36f83526960a8b10ec
AnnaKotov <anna.kotova@gmail.com>
2017-03-09 23:34:51 +0200

    change EventListActivity

--- app/src/main/java/com/kids2pull/android/adapters/EventAdapter.java
+++ app/src/main/java/com/kids2pull/android/adapters/EventAdapter.java
@@ -55,8 +55,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventHolder>
     @Override
     public void onBindViewHolder(EventHolder holder, int position) {
          holder.hobbyName.setText(Events.get(position).getEvent_id());
-        //holder.childHobby.setText(Events.get(position).getHobby_id());
-        // holder.hobbyTime.setText(Events.get(position).getEvent_date());
+         holder.hobbyTime.setText(Events.get(position).getEvent_date().toString());
         //holder.pickUp.setText(Events.get(position).getUser_ids_picker());
         // holder.drop.setText(Events.get(position).getUser_ids_spreader());
 
