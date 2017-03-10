ed37403b25f96a09060377fd7276ed030fa5a252
AnnaKotov <anna.kotova@gmail.com>
2017-03-10 13:35:37 +0200

    Merge branch 'develop' of https://github.com/livnata/Kids2Pull into develop
    # Conflicts:
    #	app/src/main/java/com/kids2pull/android/models/Hobbies_type.java

--- app/src/main/java/com/kids2pull/android/models/Pickup_type.java
+++ app/src/main/java/com/kids2pull/android/models/Pickup_type.java
@@ -1,8 +1,13 @@
 package com.kids2pull.android.models;
 
+import com.kids2pull.android.R;
+
+import java.io.Serializable;
+
 /**
  * Created by Anna on 10/03/2017.
  */
 
-public class Pickup_type {
+public enum Pickup_type implements Serializable {
+   // PICKUP_ONEWAY("PickUp_Oneway", R.drawable.group_5_copy), PICKUP_WAYBACK("PickUp_WayBack", R.drawable.group_5_copy_4);
 }
